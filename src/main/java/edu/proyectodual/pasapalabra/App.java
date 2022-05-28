024a9ba1356752e33ee8c377e35ef13540a30ad9
rafael.perez.castano <alum.rperezc.2021@iesalixar.org>
2022-05-28 11:18:19 +0200

    Initial commit

---
+++ src/main/java/edu/proyectodual/pasapalabra/App.java
@@ -0,0 +1,36 @@
+package edu.proyectodual.pasapalabra;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"));
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("")));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/edu/proyectodual/pasapalabra/controller/PrimaryController.java
@@ -0,0 +1,32 @@
+package edu.proyectodual.pasapalabra.controller;
+
+import edu.proyectodual.pasapalabra.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class PrimaryController {
+
+    @FXML
+    private TextField user;
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private void authenticate() throws IOException {
+
+        if(user.getText().equals("rafa") && password.getText().equals("123")){
+            App.setRoot("");
+        }else{
+            App.setRoot("");
+        }
+    }
+
+    @FXML
+    private void resetField(){
+        user.setText("");
+        password.setText("");
+    }
+}
