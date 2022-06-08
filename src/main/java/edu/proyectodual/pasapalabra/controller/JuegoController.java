783e86f04f5a5f311fc4a4272ac9ee3bb149dacf
rafael.perez.castano <alum.rperezc.2021@iesalixar.org>
2022-06-08 09:20:17 +0200

    commit juego completo

--- src/main/java/edu/proyectodual/pasapalabra/controller/JuegoController.java
+++ src/main/java/edu/proyectodual/pasapalabra/controller/JuegoController.java
@@ -81,6 +81,8 @@ public class JuegoController implements Initializable {
     @FXML
     private Label labelErrores;
     @FXML
+    private Label labelFin;
+    @FXML
     private Button botonRanking;
     @FXML
     private Button botonEnviar;
@@ -98,63 +100,68 @@ public class JuegoController implements Initializable {
     @FXML
     private TextField respuesta;
 
-    String[] letras = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"
-            ,"U","V","W","X","Y","Z"};
+    String[] letras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"
+            , "U", "V", "W", "X", "Y", "Z", ""};
     List<Circle> circulos;
-    int indiceLetras =0;
+    int indiceLetras = 0;
     int[] letrasPasadas = new int[26];
-    int indiceLetrasPasadas =0;
-    int cojoLetrasPasadas =0;
-    Map<String,String> mapaSaltadas = new TreeMap<>();
+    int indiceLetrasPasadas = 0;
+    int cojoLetrasPasadas = 0;
+    Map<String, String> mapaSaltadas = new TreeMap<>();
     List<String> listaPreguntas;
     List<String> listaRespuestas;
-
 
 
     @FXML
     private void rendirse() throws IOException {
         App.setRoot("logeado");
     }
+
     Juego j1 = new Juego();
     List<String> listaLetra;
     int aciertos = 0;
     int errores = 0;
-    int contadorEnvios=0;
+    int contadorEnvios = 0;
+
     @FXML
     private void comenzar() {
-        if(indiceLetras==26){
+        if (indiceLetras == 26) {
+            labelFin.setVisible(true);
             botonRanking.setVisible(true);
             System.out.println(mapaSaltadas.toString());
-
-            labelLetra.setText("FIN DEL JUEGO.");
-
+            botonEnviar.setVisible(false);
+            botonPasapalabra.setVisible(false);
+            respuesta.setVisible(false);
+            label2.setVisible(false);
+            labelLetra.setVisible(false);
+            labelFin.setTextFill(Color.WHITE);
+            labelFin.setText("FIN DEL JUEGO");
+            label.setVisible(false);
+        } else {
+            respuesta.setVisible(true);
+            botonComenzar.setVisible(false);
+            botonEnviar.setVisible(true);
+            botonPasapalabra.setVisible(true);
+            listaLetra = j1.preguntasPorLetra(letras[indiceLetras]);
+            System.out.println(listaLetra);
+            System.out.println(listaLetra.get(0));
+            System.out.println(listaLetra.get(1));
+            label.setText(listaLetra.get(0));
+            labelLetra.setText("Con la " + letras[indiceLetras]);
         }
-        respuesta.setVisible(true);
-        botonComenzar.setVisible(false);
-        botonEnviar.setVisible(true);
-        botonPasapalabra.setVisible(true);
-        listaLetra = j1.preguntasPorLetra(letras[indiceLetras]);
-        System.out.println(listaLetra);
-        System.out.println(listaLetra.get(0));
-        System.out.println(listaLetra.get(1));
-        label.setText(listaLetra.get(0));
-        labelLetra.setText("Con la "+letras[indiceLetras]);
-
     }
 
 
     @FXML
     private void pasapalabra() {
         label2.setText("");
-        if(indiceLetras==26){
 
-        }
-        mapaSaltadas.put(listaLetra.get(1),listaLetra.get(0));
+            mapaSaltadas.put(listaLetra.get(1), listaLetra.get(0));
 
-        letrasPasadas[indiceLetrasPasadas]=indiceLetras;
-        indiceLetrasPasadas++;
-        indiceLetras++;
-        comenzar();
+            letrasPasadas[indiceLetrasPasadas] = indiceLetras;
+            indiceLetrasPasadas++;
+            indiceLetras++;
+            comenzar();
 
     }
 
@@ -162,45 +169,45 @@ public class JuegoController implements Initializable {
     private void comprobarRespuesta() {
         label2.setText("");
         contadorEnvios++;
-        if(indiceLetras==26){
 
-        }
-    circulos = Arrays.asList(circuloA,circuloB,circuloC,circuloD,circuloE,circuloF,circuloG,circuloH,circuloI,circuloJ,
-            circuloK,circuloL,circuloM,circuloN,circuloO,circuloP,circuloQ,circuloR,circuloS,circuloT,circuloU,circuloV,circuloW,
-            circuloX,circuloY,circuloZ);
+            circulos = Arrays.asList(circuloA, circuloB, circuloC, circuloD, circuloE, circuloF, circuloG, circuloH, circuloI, circuloJ,
+                    circuloK, circuloL, circuloM, circuloN, circuloO, circuloP, circuloQ, circuloR, circuloS, circuloT, circuloU, circuloV, circuloW,
+                    circuloX, circuloY, circuloZ);
 
-        if(respuesta.getText().toLowerCase(Locale.ROOT).equals(listaLetra.get(1))){
-            label2.setText("");
-            aciertos++;
-            labelAciertos.setText(String.valueOf(aciertos));
-            respuesta.setText("");
-            System.out.println("correcto");
-            circulos.get(indiceLetras).setFill(Color.GREEN);
-        }else if (!respuesta.getText().toLowerCase(Locale.ROOT).equals(listaLetra.get(1))) {
-            errores++;
-            labelErrores.setText(String.valueOf(errores));
-            respuesta.setText("");
-            circulos.get(indiceLetras).setFill(Color.RED);
-            label2.setTextFill(Color.RED);
-            label2.setText("La respuesta correcta era: "+listaLetra.get(1));
-        }
-        indiceLetras++;
-        comenzar();
+            if (respuesta.getText().toLowerCase(Locale.ROOT).equals(listaLetra.get(1))) {
+                label2.setText("");
+                aciertos++;
+                labelAciertos.setText(String.valueOf(aciertos));
+                respuesta.setText("");
+                System.out.println("correcto");
+                circulos.get(indiceLetras).setFill(Color.GREEN);
+            } else if (!respuesta.getText().toLowerCase(Locale.ROOT).equals(listaLetra.get(1))) {
+                errores++;
+                labelErrores.setText(String.valueOf(errores));
+                respuesta.setText("");
+                circulos.get(indiceLetras).setFill(Color.RED);
+                label2.setTextFill(Color.RED);
+                label2.setText("La respuesta correcta era: " + listaLetra.get(1));
+            }
+            indiceLetras++;
+            comenzar();
+
 
     }
+
     @FXML
-    public void addPuntuacionARanking(){
+    public void addPuntuacionARanking() {
         Ranking ranking = new Ranking();
         ranking.setName(App.getUsuario());
-        ranking.setPoints(aciertos-errores);
+        ranking.setPoints(aciertos - errores);
         System.out.println("llega aqui");
         Ranking rankingCreado = new ClienteService(new ClienteWebService()).createRanking(ranking);
-        if (rankingCreado!= null) {
+        if (rankingCreado != null) {
             errorPuntuaciones.setTextFill(Color.RED);
             errorPuntuaciones.setText("Ha habido un error al añadir la puntuacion al ranking");
             System.out.println("llega");
             return;
-        }else{
+        } else {
             errorPuntuaciones.setText("Se han guardado las puntuaciones correctamente.");
             errorPuntuaciones.setTextFill(Color.GREEN);
         }
@@ -208,6 +215,8 @@ public class JuegoController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        labelFin.setWrapText(true);
+        labelFin.setVisible(false);
         label.setWrapText(true);
         errorPuntuaciones.setWrapText(true);
         botonRanking.setVisible(false);
